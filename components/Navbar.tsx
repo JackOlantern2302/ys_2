43ee3c1e0ff364a7325e556323178672f72c8641
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2024-07-25 16:10:16 +0700

    navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,25 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="flex p-8 justify-evenly items-center border shadow-lg bg-yellow-200">
-      <Link href={'/stock'}>Stock</Link>
-      <Link href={'/transaction'}>Transaction</Link>
+    <div className="flex py-4 px-16 justify-between items-center border shadow-lg">
+      <Image src={'/Logo.png'} alt="Logo" width={44} height={44} />
+      <div className="flex items-center gap-4">
+        <Link href={'/stock'} className="hover:font-bold transition-all">
+          Stock
+        </Link>
+        <Link href={'/transaction'} className="hover:font-bold transition-all">
+          Transaction
+        </Link>
+        <Link
+          href={'/new-order'}
+          className="bg-yellow-400 px-4 py-2 rounded-md hover:scale-[1.03] transition-all"
+        >
+          New Order
+        </Link>
+      </div>
     </div>
   );
 };
