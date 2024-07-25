0383159f222f000744def18558c2a99e57daf9f8
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2024-07-25 18:29:50 +0700

    membuat pop up tambah barang

--- app/((private))/stock/page.tsx
+++ app/((private))/stock/page.tsx
@@ -2,6 +2,7 @@ import { DataTable } from '@/components/ui/data-table';
 import { createClient } from '@/utils/supabase/server';
 import React from 'react';
 import { columns } from './columns';
+import AddItem from '@/components/AddItem';
 
 const stock = async () => {
   const supabase = createClient();
@@ -14,7 +15,11 @@ const stock = async () => {
   }
 
   return (
-    <div className="px-12 pt-8">
+    <div className="flex flex-col px-12 pt-8 gap-8">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-semibold">Stock</h1>
+        <AddItem />
+      </div>
       <DataTable columns={columns} data={stock} />
     </div>
   );
---
+++ components/AddItem.tsx
@@ -0,0 +1,84 @@
+'use client';
+import React from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
+
+import { PlusIcon } from '@radix-ui/react-icons';
+import { Button } from './ui/button';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+const AddItem = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values.username);
+  }
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <div className="bg-black rounded-md p-4 text-white">
+          <PlusIcon />
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Tambah Barang</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>Input Data</DialogDescription>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddItem;
